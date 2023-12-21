70cd838bb9d168b3b727a60114dddad2aa3f7afe
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 12:19:31 +0000

    Initial Commit

---
+++ email_slicer.py
@@ -0,0 +1,10 @@
+# Collect email
+# split the email using the @, save the first part as username
+# the second part is gonna be saved as domain
+# split the second at .
+
+def main(): 
+    print("Welcome to the email slicer.")
+    
+
+main()
