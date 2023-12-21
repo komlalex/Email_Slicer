29641db9e0717151028f1beaf8a1f40b4a88f548
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 12:20:43 +0000

    Final commit

--- email_slicer.py
+++ email_slicer.py
@@ -5,6 +5,16 @@
 
 def main(): 
     print("Welcome to the email slicer.")
-    
+    print("")
+
+    email_input = input("Input your email address: \n")
+
+    (username, domain) = email_input.split("@")
+
+    (domain, extension) = domain.split(".")
+
+    print(f"Username: {username}")
+    print(f"Domain: {domain}")
+    print(f"Extension: {extension}")
 
 main()
